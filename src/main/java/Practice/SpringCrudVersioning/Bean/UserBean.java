945345438e4988093eaa21b52b1cb5bcb79b3210
Kenit <kenit.patel@logilite.com>
2021-06-05 14:00:24 +0530

    Some Changes in Bean File

--- src/main/java/Practice/SpringCrudVersioning/Bean/UserBean.java
+++ src/main/java/Practice/SpringCrudVersioning/Bean/UserBean.java
@@ -7,7 +7,7 @@ public class UserBean {
 	private int user_id;
 	private String username;
 	private String email;
-	private BigInteger phone;
+	private String phone;
 	public int getUser_id() {
 		return user_id;
 	}
@@ -26,13 +26,13 @@ public class UserBean {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public BigInteger getPhone() {
+	public String getPhone() {
 		return phone;
 	}
-	public void setPhone(BigInteger phone) {
+	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	public UserBean(int user_id, String username, String email, BigInteger phone) {
+	public UserBean(int user_id, String username, String email, String phone) {
 		super();
 		this.user_id = user_id;
 		this.username = username;
@@ -43,5 +43,6 @@ public class UserBean {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
 		
 }
